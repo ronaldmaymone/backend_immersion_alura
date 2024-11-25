eba50245edb6d4a99d286a62b494fdd40bdb2633
Ronald <ronald.maymone@gmail.com>
2024-11-25 12:14:30 -0300

    upload img with id on local server

--- src/controllers/posts_controller.js
+++ src/controllers/posts_controller.js
@@ -1,7 +1,8 @@
+import fs from "fs";
 import {getAllPosts,createPost} from "../models/posts_model.js";
 
 export async function fetchAllPosts(req, res){
-    const allPosts = await getAllPosts()
+    const allPosts = await getAllPosts();
     res.status(200).json(allPosts);
 }
 
@@ -9,6 +10,23 @@ export async function createNewPost(req, res){
     const newPost = req.body;
     try {
         const createdPost = await createPost(newPost);
+        res.status(201).json(createdPost);
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export async function uploadImg(req, res){
+    const newPost = {
+        description: "Uploaded Image from Local Machine",
+        img_url: req.file.originalname,
+        alt: "Imagem from dog"
+    }
+    try {
+        const createdPost = await createPost(newPost);
+        const updatedImg = `uploads/${createdPost.insertedId}.png`
+        fs.renameSync(req.file.path, updatedImg);
         res.status(201).json(createdPost);
     } catch (e) {
         console.error(e.message);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,47 +1,60 @@
 import express from "express";
-import { createNewPost, fetchAllPosts } from "../controllers/posts_controller.js";
+import multer from "multer";
+import { createNewPost, fetchAllPosts, uploadImg } from "../controllers/posts_controller.js";
 
-const posts = [
-    {
-      id: 1,
-      description: "Millie",
-      image: "https://placecats.com/millie/300/150"
-    },
-    {
-      id: 2,
-      description: "Neo",
-      image: "https://placecats.com/neo/300/200"
-    },
-    {
-      id: 3,
-      description: "Millie and Neo",
-      image: "https://placecats.com/millie_neo/300/200"
-    },
-    {
-      id: 4,
-      description: "Neo 2",
-      image: "https://placecats.com/neo_2/300/200"
-    },
-    {
-      id: 5,
-      description: "Neo Banana",
-      image: "https://placecats.com/neo_banana/300/200"
-    }
-];
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+      cb(null, file.originalname);
+  }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
 
 const routes = (app) => {
     app.use(express.json());
     app.get("/api", (req, res) => {
         res.status(200).send("Hello World! This is the API endpoint. To test, visit /api");
-    })
+    });
     
-    app.get("/posts", fetchAllPosts)
+    app.get("/posts", fetchAllPosts);
     
-    app.get("/posts/:id", (req, res) => {
-        res.status(200).json(posts[req.params.id - 1]);
-    })
+    // app.get("/posts/:id", (req, res) => {
+    //     res.status(200).json(posts[req.params.id - 1]);
+    // })
 
-    app.post("/posts", createNewPost)
+    app.post("/posts", createNewPost);
+    app.post("/upload",upload.single("img"), uploadImg);
 }
 
 export default routes;
+
+// const posts = [
+//     {
+//       id: 1,
+//       description: "Millie",
+//       image: "https://placecats.com/millie/300/150"
+//     },
+//     {
+//       id: 2,
+//       description: "Neo",
+//       image: "https://placecats.com/neo/300/200"
+//     },
+//     {
+//       id: 3,
+//       description: "Millie and Neo",
+//       image: "https://placecats.com/millie_neo/300/200"
+//     },
+//     {
+//       id: 4,
+//       description: "Neo 2",
+//       image: "https://placecats.com/neo_2/300/200"
+//     },
+//     {
+//       id: 5,
+//       description: "Neo Banana",
+//       image: "https://placecats.com/neo_banana/300/200"
+//     }
+// ];
